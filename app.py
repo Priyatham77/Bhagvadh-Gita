0b87ba680530f88eb8291531c0be3a0661815c89
Priyatham77 <priyathamcoding@gmail.com>
2026-09-02 21:47:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def get_mentor_response(user_query: str):
     """
 
     response = ai_client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-1.5-flash",
         contents=prompt,
         config=types.GenerateContentConfig(
             system_instruction=SYSTEM_PROMPT,
